0416670c113a0393f63801444730024cb4f43f3d
Celina Kortmann <celina.kortmann@tu-dortmund.de>
2024-05-15 12:35:13 +0200

    plots

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: build/v46.pdf
 
-build/v46.pdf: build/BFeld.pdf v46.tex aufbau.tex auswertung.tex diskussion.tex durchfuehrung.tex fehlerrechnung.tex lit.bib theorie.tex ziel.tex | build
+build/v46.pdf: build/ndotiert1.pdf build/ndotiert2.pdf build/hochrein.pdf build/BFeld.pdf v46.tex aufbau.tex auswertung.tex diskussion.tex durchfuehrung.tex fehlerrechnung.tex lit.bib theorie.tex ziel.tex | build
 	lualatex  --output-directory=build v46.tex
 	lualatex  --output-directory=build v46.tex
 	biber build/v46.bcf
@@ -8,6 +8,15 @@ build/v46.pdf: build/BFeld.pdf v46.tex aufbau.tex auswertung.tex diskussion.tex 
 
 build/BFeld.pdf: BFeld.txt BFeld.py | build
 	python BFeld.py
+
+build/ndotiert1.pdf: Messungen1.txt ndotiert1.py | build
+	python ndotiert1.py
+
+build/ndotiert2.pdf: Messungen2.txt ndotiert2.py | build
+	python ndotiert2.py
+
+build/hochrein.pdf: Messungen3.txt hochrein.py | build
+	python hochrein.py
 
 build: 
 	mkdir -p build
---
+++ ndotiert1.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+from numpy import sqrt
+import pandas as pd
+import scipy.constants as const
+from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
+from uncertainties import ufloat                            # Fehler:           fehlerwert =  ulfaot(x, err)
+from uncertainties import unumpy as unp 
+from uncertainties.unumpy import uarray                     # Array von Fehler: fehlerarray =  uarray(array, errarray)
+from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
+                                  std_devs as stds)         # Abweichung:       stds(fehlerarray) = errarray
+
+# Plot 1:
+
+
+theta11,theta12, theta1, theta21, theta22, theta2, diff,lamb = np.genfromtxt('Messungen1.txt', unpack=True, skip_header=2)
+
+plt.plot(lamb**2, diff, 'xr', markersize=6 , label = 'Messdaten', alpha=0.5)
+plt.xlabel(r'$\lambda^2 \, / \, \mathrm{\mu m^2}$')
+plt.ylabel(r'$\theta_{norm}\, / \, \mathrm{\frac{rad}{m}}$')
+plt.legend(loc="best")                  # legend position
+plt.grid(True)                          # grid style
+
+plt.savefig('build/ndotiert1.pdf', bbox_inches = "tight")
+plt.clf() 
